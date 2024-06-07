e67950e3b5dc7703b51749b12cfcf1e054b18f7e
SeungB <ethereal0507@gmail.com>
2024-06-07 15:56:31 +0900

    logging

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 import torch
+import csv
+import os
 
 def train_model(model, trainloader, criterion, optimizer, scheduler, epochs, verbose, device, args):
     model.train()
     model.to(device)
+    loss_log = []
     for epoch in range(epochs):
         running_loss = 0.0
         for inputs, labels in trainloader:
@@ -25,4 +28,12 @@ def train_model(model, trainloader, criterion, optimizer, scheduler, epochs, ver
         scheduler.step()
         running_loss += loss.item()
         if verbose:
+            loss_log.append([epoch+1, running_loss/len(trainloader)])
             print(f'Epoch {epoch+1}, Loss: {running_loss/len(trainloader)}')
+
+    if verbose:
+        os.makedirs('log', exist_ok=True)
+        with open(f"log/{args.model}_pretrain_{args.pretrain}_{args.dataset}_{args.optimizer}_{args.learning_rate}_train_loss.csv", 'w') as f:
+            write = csv.writer(f)
+            write.writerow(loss_log)
+
