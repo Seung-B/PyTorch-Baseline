fd883efeb95a7c04329ba3715f65d6c94db96a6f
SeungB <ethereal0507@gmail.com>
2024-06-07 14:25:25 +0900

    sam

--- train.py
+++ train.py
@@ -7,15 +7,20 @@ def train_model(model, trainloader, criterion, optimizer, scheduler, epochs, ver
         running_loss = 0.0
         for inputs, labels in trainloader:
             inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
             if "SAM" in args.optimizer:
-                optimizer.first_step(zero_grad=True)
-                criterion(outputs, model(inputs)).backward()
-                optimizer.second_step(zero_grad=True)
+                def closure():
+                    loss = criterion(model(inputs), labels)
+                    loss.backward()
+                    return loss
+                loss = criterion(model(inputs), labels)
+                loss.backward()
+                optimizer.step(closure)
+                optimizer.zero_grad()
             else:
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
                 optimizer.step()
         scheduler.step()
         running_loss += loss.item()
